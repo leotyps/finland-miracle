f15fe777982d0b030e3cc1d966dc5772b41542d7
typicalsleepingboy <mpluto772@gmail.com>
2024-12-22 02:55:02 +0700

    feat:update ui hot news ⚙

--- utils/news.js
+++ utils/news.js
@@ -4,20 +4,31 @@ async function fetchHotNews() {
         const data = await response.json();
 
         const container = document.getElementById('hot-news-container');
-        container.innerHTML = ''; // Hapus skeleton loader
+        container.innerHTML = ''; 
         
         const topThreeNews = data.berita.slice(0, 3);
 
-        topThreeNews.forEach(news => {
+        let highlightedIndex = localStorage.getItem('highlightedIndex');
+        if (highlightedIndex === null) {
+            highlightedIndex = Math.floor(Math.random() * topThreeNews.length);
+            localStorage.setItem('highlightedIndex', highlightedIndex);
+        } else {
+            highlightedIndex = parseInt(highlightedIndex, 10);
+        }
+
+        topThreeNews.forEach((news, index) => {
+            const isHighlighted = index === highlightedIndex;
+            const cardClass = isHighlighted ? 'bg-blue-300 text-white' : 'bg-white text-black';
+
             const newsCard = `
-                <div class="bg-white rounded-lg shadow-md overflow-hidden p-4">
+                <div class="${cardClass} rounded-lg shadow-md overflow-hidden p-4">
                     <div class="flex items-center mb-4">
                         <img src="https://res.cloudinary.com/haymzm4wp/image/upload/assets/jkt48${news.badge_url}" alt="Badge" class="w-15 h-5 mr-3 rounded-lg">
-                        <span class="text-sm text-gray-500">${news.waktu}</span>
+                        <span class="text-sm ${isHighlighted ? 'text-gray-200' : 'text-gray-500'}">${news.waktu}</span>
                     </div>
                     <h3 class="text-lg font-bold mb-2">${news.judul}</h3>
                     <div class="flex">
-                        <a href="/news/${news.berita_id}" class="text-blue-500 hover:underline text-sm">Read More</a>
+                        <a href="/news/${news.berita_id}" class="${isHighlighted ? 'text-white' : 'text-blue-500 hover:underline'} text-sm">Read More</a>
                     </div>
                 </div>
             `;
@@ -28,5 +39,4 @@ async function fetchHotNews() {
     }
 }
 
-// Panggil fungsi untuk memuat berita
 fetchHotNews();
