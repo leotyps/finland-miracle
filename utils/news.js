b79b8ebd4080f6fdc9e9825467820e091560db32
typicalsleepingboy <mpluto772@gmail.com>
2024-12-26 01:49:56 +0700

    test

--- utils/news.js
+++ utils/news.js
@@ -1,7 +1,7 @@
 async function fetchHotNews() {
     try {
         const container = document.getElementById('hot-news-container');
-
+        
         const response = await fetch('https://intensprotectionexenew.vercel.app/api/news');
         const data = await response.json();
 
@@ -14,15 +14,16 @@ async function fetchHotNews() {
         }
 
         const topThreeNews = data.berita.slice(0, 3);
+        const colors = ["bg-blue-500", "bg-purple-500", "bg-green-500", "bg-red-500/50"];
 
-        topThreeNews.forEach((news) => {
-            const cardClass = 'bg-white text-black';
+        topThreeNews.forEach((news, index) => {
+            const colorClass = colors[index % colors.length];
 
             const newsCard = `
-                <div class="news-card ${cardClass} border-2 border-gray-200 rounded-lg overflow-hidden p-4 cursor-pointer" onclick="window.location.href='/news/${news.berita_id}'">
+                <div class="news-card ${colorClass} text-white border-2 border-gray-200 rounded-lg overflow-hidden p-4 cursor-pointer transition-all duration-300 hover:shadow-lg" onclick="window.location.href='/news/${news.berita_id}'">
                     <div class="flex items-center mb-4">
                         <img src="https://res.cloudinary.com/haymzm4wp/image/upload/assets/jkt48${news.badge_url}" alt="Badge" class="w-15 h-5 mr-3 rounded-lg">
-                        <span class="text-sm text-gray-500">${news.waktu}</span>
+                        <span class="text-sm text-white-200">${news.waktu}</span>
                     </div>
                     <h3 class="text-lg font-bold mb-2">${news.judul}</h3>
                 </div>
