0d086d56cb5ddc4abe1a5eef38e6ea5f007738ba
typicalsleepingboy <mpluto772@gmail.com>
2024-12-26 01:57:10 +0700

    feat: fix typo pathSegments[pathSegments.length - 1]

--- utils/detail-member.js
+++ utils/detail-member.js
@@ -7,7 +7,8 @@ async function fetchDetailMember() {
   const container = document.getElementById("member-detail-container");
 
   try {
-    const memberId = getQueryParam("id");
+    const pathSegments = window.location.pathname.split('/');
+    const memberId = pathSegments[pathSegments.length - 1];
     if (!memberId) {
       container.innerHTML = `
         <div class="flex items-center justify-center h-96">
