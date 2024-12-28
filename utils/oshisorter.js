c22ffa87ac4f11f8fa251bcc687ccdbc05ffc32b
typicalsleepingboy <mpluto772@gmail.com>
2024-12-29 00:40:02 +0700

    fix some ui

--- utils/oshisorter.js
+++ utils/oshisorter.js
@@ -53,7 +53,7 @@ async function loadMembers() {
     const filterButtonsContainer = document.getElementById('filter-buttons');
 
     const genList = document.createElement('div');
-    genList.className = 'grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4';
+    genList.className = 'grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4';
 
     generations.forEach(gen => {
         const button = document.createElement('button');
