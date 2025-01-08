9ad3546a1a5d63ce395f3471af9e504cb966aa1d
typicalsleepingboy <mpluto772@gmail.com>
2025-01-08 12:18:49 +0700

    Feat: add custom banner and fix performance

--- utils/banner.js
+++ utils/banner.js
@@ -3,6 +3,7 @@ fetch('https://intensprotectionexenew.vercel.app/api/banners')
     .then(data => {
         if (data.success && data.data) {
             const bannerSlider = document.querySelector('.banner-slider');
+            
             data.data.forEach(banner => {
                 const link = document.createElement('a');
                 link.href = banner.value;
@@ -15,6 +16,29 @@ fetch('https://intensprotectionexenew.vercel.app/api/banners')
 
                 link.appendChild(img);
                 bannerSlider.appendChild(link);
+            });
+
+
+            const customBanners = [
+                { url: 'https://48intens.com/discord', img: 'https://res.cloudinary.com/dlx2zm7ha/image/upload/v1736313014/cjzknqpzc7eokiwyrufh.webp' },
+                { url: 'https://48intens.com/discord', img: 'https://res.cloudinary.com/dlx2zm7ha/image/upload/v1736313244/znakglqjkjxzrddah4od.webp' },
+                { url: 'https://48intens.com/discord', img: 'https://res.cloudinary.com/dlx2zm7ha/image/upload/v1736313480/ilfgvvkbwdlx9kjshfn1.webp' }
+            ];
+
+            customBanners.forEach(customBanner => {
+                if (customBanner.url && customBanner.img) {
+                    const link = document.createElement('a');
+                    link.href = customBanner.url;
+                    link.className = 'block w-full h-full';
+
+                    const img = document.createElement('img');
+                    img.src = customBanner.img;
+                    img.alt = 'Custom Banner';
+                    img.className = 'w-full h-full object-cover';
+
+                    link.appendChild(img);
+                    bannerSlider.appendChild(link);
+                }
             });
 
             tns({
