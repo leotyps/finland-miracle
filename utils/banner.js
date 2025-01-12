7fef9b3b68aa76e32b916d2563f7c49b9912c970
typicalsleepingboy <mpluto772@gmail.com>
2025-01-12 10:37:29 +0700

    update some ui

--- utils/banner.js
+++ utils/banner.js
@@ -21,7 +21,7 @@ fetch('https://intensprotectionexenew.vercel.app/api/banners')
 
             const customBanners = [
                 { url: 'https://48intens.com/discord', img: 'https://res.cloudinary.com/dlx2zm7ha/image/upload/v1736313014/cjzknqpzc7eokiwyrufh.webp' },
-                { url: 'https://jkt48.com', img: 'https://cdn.discordapp.com/attachments/1189322953331048558/1327813240331178035/anniv_copy.webp?ex=67846df2&is=67831c72&hm=635aa2b771feabdec755738cf54d1e6eaa30c554b4e625380b3d625e67f47276&' },
+                { url: 'https://jkt48.com', img: 'https://cdn.discordapp.com/attachments/1189322953331048558/1327843282113265665/GgJPm8WbYAIP0nS.jpeg?ex=678489ec&is=6783386c&hm=7bd2dbc12337bd9d01a11927e5f399a4aab11a1f66e41d25e27fe73628453617&' },
                 { url: 'https://cdn.discordapp.com/attachments/1309037991888683018/1316772733211902033/1211_38.mp4?ex=67827f2c&is=67812dac&hm=df80e31b65e9b07288db0d856ddc983c979e15271c9ab30fef221e1514af6f80&', img: 'https://res.cloudinary.com/dlx2zm7ha/image/upload/v1736313244/znakglqjkjxzrddah4od.webp' },
                 { url: 'https://48intens.com/discord', img: 'https://cdn.discordapp.com/attachments/1189322953331048558/1327814113958690868/anniv_ffcopy.webp?ex=67846ec2&is=67831d42&hm=11931ac732ec62f58adddc433b1e693c541112c547d8be3dda6a251ca9f70c1e&' }
             ];
