3f6a59ce87b5f70cbbe61f22e6e5572bd282fd62
Typicalsleepingboy <typicalsleepingboyxxx@gmail.com>
2025-03-09 05:58:34 +0700

    Feat: refactor live.js and add live jkt48v

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
-tailwind.config = {
-    darkMode: 'class',
+module.exports = {
     theme: {
-        extend: {}
-    }
-}
+        extend: {
+            colors: {
+                'pastel-mint': '#B2F2BB',
+                'pastel-peach': '#FFD8B2',
+                'pastel-pink': '#FFC3C3',
+                'pastel-pink-dark': '#FFA7A7',
+                'pastel-green': '#C1E1C1',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
